cb83940cca35a428f0a7e18c53e948561fb5632b
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2023-01-23 16:58:57 -0500

    Create object schema with Mongoose; populate MongoDB

---
+++ .seedgooserc.js
@@ -0,0 +1,7 @@
+// .seedgooserc.js
+module.exports = {
+    modelBaseDirectory: "app_server/database/models",
+    models: ["*.js", "!db.js"],
+    data: "data",
+    db: "mongodb://127.0.0.1:27017/travlr",
+};
