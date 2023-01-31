10b3c3343b6d02f9efab72e053e8adb792f8fa4b
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2023-01-31 13:46:40 -0500

    Create RESTful API

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 // .seedgooserc.js
 module.exports = {
-    modelBaseDirectory: "app_server/database/models",
+    modelBaseDirectory: "app_api/database/models",
     models: ["*.js", "!db.js"],
     data: "data",
     db: "mongodb://127.0.0.1:27017/travlr",
