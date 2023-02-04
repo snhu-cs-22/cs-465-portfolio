d66967af9c064431721ff006bebf319c298fadd1
snhu-cs-22 <110997198+snhu-cs-22@users.noreply.github.com>
2023-02-04 13:48:10 -0500

    Change travel page to use RESTful API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,48 @@
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+var request = require('request');
+var apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-/* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+/* Render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = `${process.env.npm_package_description}\t:\tTravel`;
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else if (!responseBody.length) {
+            message = 'No trips exist in database!';
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody, message,
+    });
+}
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    }
+
+    console.info(`>> travelController.travelList calling ${requestOptions.url}`);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
 };
 
 module.exports = {
-    travel
+    travelList
 };
